9f6372ac9f1e9165cbe24d639b8c63b6939eda97
Christian Bär <teggno@hotmail.com>
2020-11-03 16:57:40 +0100

    fix time display

--- src/time.js
+++ src/time.js
@@ -2,9 +2,9 @@ import { padStart } from "./strings";
 
 export function secondsToTimeString(seconds) {
   const hours = Math.floor(seconds / 60 / 60);
-  const minutes = Math.ceil(seconds / 60);
+  const minutes = Math.floor(seconds / 60) % 60;
   const remainingSeconds = seconds % 60;
   return `${hours ? hours.toString() + ":" : ""}${
-    hours || minutes ? padStart(minutes, 2, "0") + ":" : ""
-  }${hours || minutes ? padStart(remainingSeconds, 2, "0") : seconds}`;
+    padStart(minutes, 2, "0") + ":"
+  }${padStart(remainingSeconds, 2, "0")}`;
 }
